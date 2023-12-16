ae6822efcc8671fd55aa627ec9667e53dc6bb61b
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-12-16 17:39:21 +0600

    Matplotlib #6 Случайное блуждание с запросом на повтор

--- rw_visual.py
+++ rw_visual.py
@@ -2,12 +2,16 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 # построение случайного блуждания
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Нанеение точек на диаграму
-plt.style.use("ggplot")
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=5)
-plt.savefig('MPL_points.jpg', bbox_inches='tight')
-plt.show()
+    # Нанеение точек на диаграму
+    plt.style.use("ggplot")
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=5)
+    plt.savefig('MPL_points.jpg', bbox_inches='tight')
+    plt.show()
+    keep_running = input("Make anower walk? (y/n)")
+    if keep_running == 'n':
+        break
