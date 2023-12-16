3189920b3beefc37f3fb0d6ca64d5f6358e89ce4
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-12-16 17:50:11 +0600

    Matplotlib #8 Случайное блуждание с запросом на повтор + градиент + начальная и конечная точка

--- rw_visual.py
+++ rw_visual.py
@@ -6,13 +6,20 @@ while True:
     rw = RandomWalk()
     rw.fill_walk()
 
-    # Нанеение точек на диаграму
+    # нанеение точек на диаграму
     plt.style.use("ggplot")
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, s=10, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
     plt.savefig('MPL_points.jpg', bbox_inches='tight')
+
+    # выделение первой и последней точки
+    ax.scatter(0, 0, c='green', s=50, cmap=plt.cm.Blues, edgecolors='none')
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
     plt.show()
+
+
+
     keep_running = input("Make anower walk? (y/n)")
     if keep_running == 'n':
         break
